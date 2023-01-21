9b9bd892ea1ca52d8b424a6ed46a8a43081c9021
Lux14 <116319972+Lux-14@users.noreply.github.com>
2023-01-21 18:07:28 -0500

    Update Form1.cs

--- DesktopLocker/Form1.cs
+++ DesktopLocker/Form1.cs
@@ -1,44 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
-
-namespace DesktopLocker
-{
-    public partial class Form1 : Form
-    {
-        private readonly string pass = "unlock";
-        private bool check = true;
-
-        public Form1()
-        {
-            InitializeComponent();
-            this.Height = Screen.PrimaryScreen.Bounds.Height;
-            this.Width = Screen.PrimaryScreen.Bounds.Width;
-            this.textBox1.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - 140, Screen.PrimaryScreen.Bounds.Height / 2);
-            this.button1.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2 + 50);
+namespace DesktopLocker{
+    public partial class Form1:Form{
+        private readonly string pass="unlock";
+        private bool check=true;    
+        public Form1(){
+            InitializeComponent();   
+            this.Height=Screen.PrimaryScreen.Bounds.Height;
+            this.Width=Screen.PrimaryScreen.Bounds.Width;
+            this.textBox1.Location=new Point(Screen.PrimaryScreen.Bounds.Width/2-140,Screen.PrimaryScreen.Bounds.Height/2);
+            this.button1.Location=new Point(Screen.PrimaryScreen.Bounds.Width/2,Screen.PrimaryScreen.Bounds.Height/2+50);
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "unlock")
-            {
-                check = false;
-                Application.Exit();
-            }
-        }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (check)
-            {
-                e.Cancel = true;
-            }
-        }
+        private void button1_Click(object sender,EventArgs e){if(textBox1.Text=="unlock"){check=false;Application.Exit();}}
+        private void Form1_FormClosing(object sender,FormClosingEventArgs e){if(check){e.Cancel = true;}}
     }
 }
